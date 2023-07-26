9bfe6d3f7d750b4264c64d213fafd182aca5a7f2
Suresh Kumar <suresh.uncbc.coding@gmail.com>
2023-07-25 22:58:15 -0400

    build code for placeOrder method -1

--- spring-boot-online-store/src/main/java/com/perscholas/ecommerce/service/CheckoutServiceImpl.java
+++ spring-boot-online-store/src/main/java/com/perscholas/ecommerce/service/CheckoutServiceImpl.java
@@ -1,11 +1,25 @@
 package com.perscholas.ecommerce.service;
 
+import com.perscholas.ecommerce.dao.CustomerRepository;
 import com.perscholas.ecommerce.dto.Purchase;
 import com.perscholas.ecommerce.dto.PurchaseResponse;
+import com.perscholas.ecommerce.entity.Order;
+import org.springframework.beans.factory.annotation.Autowired;
 
 public class CheckoutServiceImpl implements CheckoutService{
+
+    private CustomerRepository customerRepository;
+
+    @Autowired
+    public CheckoutServiceImpl(CustomerRepository customerRepository) {
+        this.customerRepository = customerRepository;
+    }
+
     @Override
     public PurchaseResponse placeOrder(Purchase purchase) {
+        // retrieve the order info from dto
+        Order order = purchase.getOrder();
+
         return null;
     }
 }
