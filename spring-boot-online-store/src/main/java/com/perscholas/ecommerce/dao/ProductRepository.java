48f26309d66209ad0138322f5231c33a5ae86b2f
Suresh Kumar <suresh.uncbc.coding@gmail.com>
2023-08-15 22:39:03 -0400

    refactor

--- spring-boot-online-store/src/main/java/com/perscholas/ecommerce/dao/ProductRepository.java
+++ spring-boot-online-store/src/main/java/com/perscholas/ecommerce/dao/ProductRepository.java
@@ -5,11 +5,13 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.web.bind.annotation.CrossOrigin;
 // add cross origin support using annotation
 //The @CrossOrigin annotation is used to enable Cross-Origin Resource Sharing
 // (CORS) in a Spring Boot application. CORS is a mechanism that allows a web application running on one domain to make requests to a resource on another domain.
-//@CrossOrigin("http://localhost:4200")
+//@CrossOrigin("http://
+@RepositoryRestResource
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
     // additional custom query methods based on specific search/query criteria
